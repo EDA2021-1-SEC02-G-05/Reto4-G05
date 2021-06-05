e8dd809f21515f194e262a1f0ae7b1eb6842c250
svela0426 <74679743+svela0426@users.noreply.github.com>
2021-06-05 12:54:28 -0500

    avance

--- App/model.py
+++ App/model.py
@@ -27,6 +27,8 @@
 
 from os import name
 import config as cf
+import re
+import haversine as hs
 from DISClib.ADT import list as lt
 from DISClib.ADT.graph import gr
 from DISClib.ADT import map as mp
@@ -34,8 +36,8 @@ from DISClib.DataStructures import mapentry as me
 from DISClib.Algorithms.Sorting import shellsort as sa
 from DISClib.Algorithms.Graphs import dijsktra as djk
 from DISClib.Algorithms.Graphs import scc as kosa
-from DISClib.Algorithms.Graphs import prim as mst
 from DISClib.Algorithms.Graphs import bfs as bfs
+from DISClib.DataStructures import listiterator as it
 from DISClib.Utils import error as error
 assert cf
 
@@ -52,49 +54,143 @@ def newgraph():
     dicci={}
 
     dicci["grafico"]=gr.newGraph(datastructure='ADJ_LIST',directed= False,size=14000,comparefunction=None)
+    dicci["grafico2"]=gr.newGraph(datastructure='ADJ_LIST',directed= False,size=14000,comparefunction=None)
     dicci["lista"]=lt.newList()
+    dicci["tabla"]=mp.newMap()
     dicci["paises"]=lt.newList()
 
     dicci["mayores"]=mp.newMap()
     return dicci
 
 def agregarvertices(dicci,ver):
-
-    gr.insertVertex(dicci["grafico"],ver["landing_point_id"])
-    lt.addLast(dicci["lista"],ver)
+        
+    lis=[]
+    lola=[]
+    lis.append(lola)
+    lis[0].append(ver)
+    mp.put(dicci["tabla"],ver["landing_point_id"],lis)
     
+    
+    return dicci
+
+
+def union(dicci,verti):
+
+    elbicho = verti["cable_name"]
+    
+   
+
+    vela = str(verti["origin"])+"-"+str(elbicho)
+
+    jef= mp.get(dicci["tabla"],verti["origin"])
+    lis = me.getValue(jef)
+    lis.append(vela) 
+
+    vela2 = str(verti["destination"])+"-"+str(elbicho)
+    jef2= mp.get(dicci["tabla"],verti["destination"])
+    lis2 = me.getValue(jef2)
+    lis2.append(vela2) 
+
+
+
+
+    return dicci
+   
+
+
+
+
+
+
+
+
+
+def completo(dicci, verti):
+
+    kun= verti["cable_name"]
+    
+    
+    vela = str(verti["origin"])+"-"+str(kun)
+    riquelme=str(verti["destination"])+"-"+str(kun)
+
+    gr.insertVertex(dicci["grafico"],vela)
+    gr.insertVertex(dicci["grafico"],riquelme)
+
 
     return dicci
 
-def agregararcos(dicci,verti):
+
+
+def completo1(dicci, verti):
 
     elbicho = verti["cable_length"]
+    kun= verti["cable_name"]
 
-    vela = str(verti["origin"])+""
     
     if elbicho == "n.a.":
 
         elbicho = 0
+        vela = str(verti["origin"])+"-"+str(kun)
+        riquelme=str(verti["destination"])+"-"+str(kun)
 
-        gr.addEdge(dicci['grafico'],verti["destination"],verti["origin"],elbicho)
+        gr.addEdge(dicci['grafico'],vela,riquelme,float(elbicho))
+
+        
+
 
     else:
 
-        elbicho = elbicho[:-3]
+        elbicho1 = elbicho[:-3]
 
-        elbicho = elbicho.replace(",","")
+        elbicho1 = elbicho1.replace(",","")
 
-        elbicho = float(elbicho)
+        vela1 = str(verti["origin"])+"-"+str(kun)
+        riquelme1=str(verti["destination"])+"-"+str(kun)
 
-        gr.addEdge(dicci['grafico'],verti["destination"],verti["origin"],elbicho)
+        gr.addEdge(dicci['grafico'],vela1,riquelme1,float(elbicho1))
+
 
     return dicci
+
+
+
+
 
 def agregarpaises(dicci,pp):
 
     lt.addLast(dicci["paises"],pp)
 
     return dicci
+
+
+
+def mapa(dicci):
+    
+
+
+    chabal= mp.valueSet(dicci["tabla"])
+
+    iterador1 = it.newIterator(chabal)
+
+    while it.hasNext(iterador1):
+
+        actual1 = it.next(iterador1)
+
+        ronney=None
+
+        for i in actual1[1:]:
+
+            if ronney == None:
+
+                ronney=i
+
+            else:
+                gr.addEdge(dicci['grafico'],ronney,i,0.1)
+                ronney= i
+
+    return dicci
+       
+ 
 
 def requerimiento1(dicci,lad1,lad2):
 
@@ -108,40 +204,86 @@ def requerimiento1(dicci,lad1,lad2):
 
 
 def requerimiento2(dicci):
+    lista = []
 
-    for i in range(0,lt.size(dicci["lista"])):
+    chabal= mp.keySet(dicci["tabla"])
 
-        rta=lt.getElement(dicci["lista"],i)
+    iterador1 = it.newIterator(chabal)
 
-        numero = gr.degree(dicci["grafico"],rta["landing_point_id"])
+    while it.hasNext(iterador1):
 
-        lista = []
+        actual1 = it.next(iterador1)
 
-        lista.append(("Nombre del pais: "+str(rta["name"])+" con el identificador: "+str(rta["id"])+" con su numero total de conexiones es: "+str(numero)))
+        jef2= mp.get(dicci["tabla"],actual1)
+        lis2 = me.getValue(jef2)
 
-        mp.put(dicci["mayores"],rta["name"],lista)
+        numero= gr.degree(dicci["grafico"],lis2[1])
 
-    valoresHash=mp.valueSet(dicci["mayores"])
 
-    top1 = lt.getElement(valoresHash,1)
-    top2 = lt.getElement(valoresHash,2)
-    top3 = lt.getElement(valoresHash,3)
-    top4 = lt.getElement(valoresHash,4)
-    top5 = lt.getElement(valoresHash,5)
+        lista.append(("Nombre del pais: "+str(lis2[0][0]["name"])+" con el identificador: "+str(lis2[1])+", con su numero total de conexiones es: "+str(numero)))
 
-    return top1,top2,top3,top4,top5
+
+
+        
+ 
+
+    return lista
 
 
 def requerimiento3(dicci,paisA,paisB):
 
+    lili=[]
+    lola=[]
+
     Rutas=djk.Dijkstra(dicci["grafico"],paisA)
 
-    Minima = djk.distTo(Rutas,paisB)
 
     camino = djk.pathTo(Rutas,paisB)
 
-  
-    return Minima,camino
+    iterador1 = it.newIterator(camino)
+
+    while it.hasNext(iterador1):
+
+        actual1 = it.next(iterador1)
+
+
+        coco=actual1["vertexA"]
+        coco = (coco.split("-"))[0] 
+        lili.append(coco)
+
+    jef2= mp.get(dicci["tabla"],lili[0])
+    lis2 = me.getValue(jef2)
+
+    lat=float(lis2[0][0]["latitude"])
+    log=float(lis2[0][0]["longitude"])
+
+
+    jef= mp.get(dicci["tabla"],lili[1])
+    lis = me.getValue(jef)
+
+    lat1=float(lis[0][0]["latitude"])
+    log1=float(lis[0][0]["longitude"])
+
+
+
+
+    lol= (lat,log)
+    lol2=(lat1,log1)
+    dm=hs.haversine(lol,lol2)
+
+
+
+
+    
+
+
+    return lili
+
+
+
+
+
+
 
 
 def requerimiento4(dicci):
@@ -159,20 +301,7 @@ def requerimiento4(dicci):
 
 
 
-
         
- 
-
-
-
-
-
-
-
-
-        
-
-
 
 
     
