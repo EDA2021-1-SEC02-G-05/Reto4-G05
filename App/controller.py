32651b9105f01432cbbabf034f77984e06a9fc0e
jngm8 <68575075+jngm8@users.noreply.github.com>
2021-06-06 23:42:42 -0500

    Entrega Final Reto 4

--- App/controller.py
+++ App/controller.py
@@ -1,25 +1,3 @@
-﻿"""
- * Copyright 2020, Departamento de sistemas y Computación,
- * Universidad de Los Andes
- *
- *
- * Desarrolado para el curso ISIS1225 - Estructuras de Datos y Algoritmos
- *
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along withthis program.  If not, see <http://www.gnu.org/licenses/>.
- """
-
 import config as cf
 import model
 import csv
@@ -129,19 +107,19 @@ def loadrequerimiento3(dicci,paisA,paisB):
 
     return req3
 
+
 def loadrequerimiento4(dicci):
 
     req3 = model.requerimiento4(dicci)
 
-    return req3
+    return req3[0],req3[1],req3[2]
 
 
 def loadrequerimiento5(dicci,mache):
 
     return model.requerimiento5(dicci,mache)
 
-# Funciones para la carga de datos
 
-# Funciones de ordenamiento
+def loadrequerimiento6(dicci,pais,cable):
 
-# Funciones de consulta sobre el catálogo
+    return model.requerimiento6(dicci,pais,cable)
